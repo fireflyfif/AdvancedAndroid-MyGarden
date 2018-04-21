636932220a3d2d686a9cb086747d3901ffd530b3
fireflyfif <fif.iva@gmail.com>
2018-04-21 20:44:01 +0200

    completed all TODOs for TWID.03-Exercise-UpdateWidgetsService

--- app/src/main/java/com/example/android/mygarden/ui/AddPlantActivity.java
+++ app/src/main/java/com/example/android/mygarden/ui/AddPlantActivity.java
@@ -24,6 +24,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.android.mygarden.PlantWateringService;
+import com.example.android.mygarden.PlantWidgetProvider;
 import com.example.android.mygarden.R;
 import com.example.android.mygarden.provider.PlantContract;
 
@@ -64,6 +66,8 @@ public class AddPlantActivity extends AppCompatActivity {
         contentValues.put(PlantContract.PlantEntry.COLUMN_CREATION_TIME, timeNow);
         contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
         getContentResolver().insert(PlantContract.PlantEntry.CONTENT_URI, contentValues);
+
+        PlantWateringService.startActionUpdatePlantWidgets(this);
         // Close this activity
         finish();
     }
